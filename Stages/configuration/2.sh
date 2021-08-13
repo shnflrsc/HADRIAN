ed7f94330628f6248f5a3e27ad2006f072d59b55
furesuka <floresca.shanegarcia@gmail.com>
2021-08-13 14:00:19 +0800

    Use Roboto Mono in Fix Terminal Fonts

--- Stages/configuration/2.sh
+++ Stages/configuration/2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 clear
-sudo pacman -S ttf-droid --noconfirm
-gsettings set org.gnome.desktop.interface monospace-font-name 'Droid Sans Mono Regular 10'
+sudo pacman -S ttf-roboto-mono --noconfirm
+gsettings set org.gnome.desktop.interface monospace-font-name 'Roboto Mono 10'
 
 echo 
 read -p "Fix Terminal Font - Done!"
