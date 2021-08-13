75347f0d1d1dbc7f9b4098d07e37647d1de26f9a
furesuka <floresca.shanegarcia@gmail.com>
2021-08-13 09:40:33 +0800

    Remove END stage

--- Stages/end/END.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-dialog \
---clear \
---backtitle "HADRIAN: End" \
---title "End of Installation" \
---msgbox "You have finished the installation process of HADRIAN.\n\nIt is recommended to reboot your PC after exiting.\nEnjoy!" 8 70
