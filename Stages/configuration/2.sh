fa08cc948fd4e3bd8cec7d4f5a30706340818eab
furesuka <floresca.shanegarcia@gmail.com>
2021-08-14 11:49:43 +0800

    Actual Default Fonts

--- Stages/configuration/2.sh
+++ Stages/configuration/2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 clear
-sudo pacman -S ttf-droid ttf-roboto-mono --noconfirm
+sudo pacman -S inter-font ttf-opensans ttf-roboto-mono --noconfirm
+gsettings set org.gnome.desktop.interface font-name 'Inter 9'
+gsettings set org.gnome.desktop.interface document-font-name 'Open Sans 10'
 gsettings set org.gnome.desktop.interface monospace-font-name 'Roboto Mono 10'
 
 echo 
